5ea065de171f9f1554664de36d8abfaaaa8c4c5e
Daniel <opodaniel@yahoo.com>
2020-07-24 09:21:26 +0300

    initial commit - create react app - create Pallete component - use styled components for styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Hello World of colors !!!</h3>
+      <Palette palette={{...seedColors[0]}}/>
     </div>
   );
 }
---
+++ src/Palette.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from 'styled-components';
+
+const PaletteStyled = style.div`
+
+`;
+
+const PaletteColorsStyled = style.div`
+
+`;
+
+const Palette = () => { 
+    return (
+        <PaletteStyled>
+            
+            <PaletteColorsStyled>
+                colors
+            </PaletteColorsStyled>
+        </PaletteStyled>
+    );
+};
+
+export default Palette;
